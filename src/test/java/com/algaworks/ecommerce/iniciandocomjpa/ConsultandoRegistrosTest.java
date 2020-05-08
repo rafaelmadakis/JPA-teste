6aa9a212a5903afba9183042786356f2484f762a
rafaelmadakis <rafaelmadakis@gmail.com>
2020-05-07 21:53:43 -0300

    Buscar objetos por identificador

--- src/test/java/com/algaworks/ecommerce/iniciandocomjpa/ConsultandoRegistrosTest.java
+++ src/test/java/com/algaworks/ecommerce/iniciandocomjpa/ConsultandoRegistrosTest.java
@@ -1,9 +1,7 @@
 package com.algaworks.ecommerce.iniciandocomjpa;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
+import com.algaworks.ecommerce.model.Produto;
+import org.junit.*;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -34,5 +32,31 @@ public class ConsultandoRegistrosTest {
     @After
     public void tearDown() {
         entityManager.close();
+    }
+
+    /**
+     * Buscar por identificador
+     */
+    @Test
+    public void buscarIdentificador() {
+
+        Produto produto = entityManager.find(Produto.class, 1);
+//        Produto produto = entityManager.getReference(Produto.class, 1);
+
+        Assert.assertNotNull(produto);
+        Assert.assertEquals("Kindle", produto.getNome());
+    }
+
+    /**
+     * Reiniciar Entidade
+     */
+    @Test
+    public void atualizarAReferencia() {
+        Produto produto = entityManager.find(Produto.class, 1);
+        produto.setNome("Microfone Samson");
+
+        entityManager.refresh(produto);
+
+        Assert.assertEquals("Kindle", produto.getNome());
     }
 }
