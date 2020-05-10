153f94df577a7127e884ec48393d9e8ef7fb297a
rafaelmadakis <rafaelmadakis@gmail.com>
2020-05-10 20:44:19 -0300

    Mapeando as entidades e customizando os nomes das tabelas e colunas

--- src/main/java/com/algaworks/ecommerce/model/Categoria.java
+++ src/main/java/com/algaworks/ecommerce/model/Categoria.java
@@ -4,13 +4,16 @@ import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Getter
 @Setter
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
+@Table(name = "categoria")
 public class Categoria {
 
     @EqualsAndHashCode.Include
@@ -19,6 +22,7 @@ public class Categoria {
 
     private String nome;
 
+    @Column(name = "categoria_pai_id")
     private Integer categoriaPaiId;
 
 }
