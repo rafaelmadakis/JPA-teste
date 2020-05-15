8a7639744497b8a15451637bf941216b47c808d1
rafaelmadakis <rafaelmadakis@gmail.com>
2020-05-14 21:57:30 -0300

    usando o atributo optional na classe de teste mapeamentoObjetoEmbutidoTest(erro transient)

--- src/test/java/com/algaworks/ecommerce/mapeamentobasico/MapeamentoObjetoEmbutidoTest.java
+++ src/test/java/com/algaworks/ecommerce/mapeamentobasico/MapeamentoObjetoEmbutidoTest.java
@@ -1,6 +1,7 @@
 package com.algaworks.ecommerce.mapeamentobasico;
 
 import com.algaworks.ecommerce.EntityManagerTest;
+import com.algaworks.ecommerce.model.Cliente;
 import com.algaworks.ecommerce.model.EnderecoEntregaPedido;
 import com.algaworks.ecommerce.model.Pedido;
 import com.algaworks.ecommerce.model.StatusPedido;
@@ -14,8 +15,10 @@ public class MapeamentoObjetoEmbutidoTest extends EntityManagerTest {
 
     @Test
     public void analisarMapeamentoObjetoEmbutido() {
+        Cliente cliente = entityManager.find(Cliente.class, 1);
+
         EnderecoEntregaPedido endereco = new EnderecoEntregaPedido();
-        endereco.setCep("11075-221");
+        endereco.setCep("00000-000");
         endereco.setLogradouro("Rua das Laranjeiras");
         endereco.setNumero("123");
         endereco.setBairro("Centro");
@@ -28,6 +31,7 @@ public class MapeamentoObjetoEmbutidoTest extends EntityManagerTest {
         pedido.setStatus(StatusPedido.AGUARDANDO);
         pedido.setTotal(new BigDecimal(1000));
         pedido.setEnderecoEntrega(endereco);
+        pedido.setCliente(cliente);
 
         entityManager.getTransaction().begin();
         entityManager.persist(pedido);
