e948e1c2dce5d06a10c0530a417747faf457f9a3
rafaelmadakis <rafaelmadakis@gmail.com>
2020-05-20 20:04:05 -0300

    Mapeando chave primária e estrangeira na mesma coluna com @MapsId

---
+++ src/test/java/com/algaworks/ecommerce/mapeamentoavancado/MapsIdTest.java
@@ -0,0 +1,63 @@
+package com.algaworks.ecommerce.mapeamentoavancado;
+
+import com.algaworks.ecommerce.EntityManagerTest;
+import com.algaworks.ecommerce.model.*;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+public class MapsIdTest extends EntityManagerTest {
+
+    @Test
+    public void inserirPagamento() {
+        Pedido pedido = entityManager.find(Pedido.class, 1);
+
+        NotaFiscal notaFiscal = new NotaFiscal();
+//        notaFiscal.setId(pedido.getId()); @MapsId
+        notaFiscal.setPedido(pedido);
+        notaFiscal.setDataEmissao(new Date());
+        notaFiscal.setXml("<xml/>");
+
+        entityManager.getTransaction().begin();
+        entityManager.persist(notaFiscal);
+        entityManager.getTransaction().commit();
+
+        entityManager.clear();
+
+        NotaFiscal notaFiscalVerificacao = entityManager.find(NotaFiscal.class, notaFiscal.getId());
+        Assert.assertNotNull(notaFiscalVerificacao); // verifica se o pedido foi inserido
+        Assert.assertEquals(pedido.getId(), notaFiscalVerificacao.getId()); // veirifica se o identificador da nota fiscal é o mesmo do pedido
+    }
+
+    @Test
+    public void inserirItemPedido() {
+        Cliente cliente = entityManager.find(Cliente.class, 1);
+        Produto produto = entityManager.find(Produto.class, 1);
+
+        Pedido pedido = new Pedido();
+        pedido.setCliente(cliente);
+        pedido.setDataCriacao(LocalDateTime.now());
+        pedido.setStatus(StatusPedido.AGUARDANDO);
+        pedido.setTotal(produto.getPreco());
+
+        ItemPedido itemPedido = new ItemPedido();
+        itemPedido.setId(new ItemPedidoId());
+        itemPedido.setPedido(pedido);
+        itemPedido.setProduto(produto);
+        itemPedido.setPrecoProduto(produto.getPreco());
+        itemPedido.setQuantidade(1);
+
+        entityManager.getTransaction().begin();
+        entityManager.persist(pedido);
+        entityManager.persist(itemPedido);
+        entityManager.getTransaction().commit();
+
+        entityManager.clear();
+
+        ItemPedido itemPedidoVerificacao = entityManager.find(
+                ItemPedido.class, new ItemPedidoId(pedido.getId(), produto.getId()));
+        Assert.assertNotNull(itemPedidoVerificacao);
+    }
+}
