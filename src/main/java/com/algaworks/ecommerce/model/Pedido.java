e05d19c523b7ca467a636b2d4014d6a3b3d488ff
rafaelmadakis <rafaelmadakis@gmail.com>
2020-06-10 23:10:11 -0300

    Fazendo inserções de objetos em cascata

--- src/main/java/com/algaworks/ecommerce/model/Pedido.java
+++ src/main/java/com/algaworks/ecommerce/model/Pedido.java
@@ -27,7 +27,7 @@ public class Pedido extends EntidadeBaseInteger {
     @JoinColumn(name = "cliente_id", nullable = false, foreignKey = @ForeignKey(name = "fk_pedido_cliente"))
     private Cliente cliente;
 
-    @OneToMany(mappedBy = "pedido", cascade = CascadeType.REFRESH)
+    @OneToMany(mappedBy = "pedido")
     private List<ItemPedido> itens;
 
     @Column(name = "data_criacao", updatable = false, nullable = false)
